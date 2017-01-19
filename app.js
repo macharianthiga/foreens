0945c392c3348800ce6aadc01b1b2eab46d12bda
dennisfromkenya <dmdmasha28@gmail.com>
2017-01-19 08:08:39 +0000

    finish setting up the about page

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require("express"),
 app.set("view engine","ejs");
 app.use(express.static('public'));
 
-
+//--------------Index Route---------------
 app.get("/", function(req, res){
   res.render("index");
 });
 
+//-------------About Route-----------------
+app.get('/about', function(req,res){
+   res.render('about');
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Foreens Has Started");
